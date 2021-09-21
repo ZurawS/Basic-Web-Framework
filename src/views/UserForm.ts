97ca5ab8bcab17833c37fa2714ea1cec191efc48
PawZet <pawelzurawski898@gmail.com>
2021-09-21 23:00:41 +0200

    commit_6

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,13 +1,29 @@
 import { User } from "./../models/User";
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
+    if (!input) return;
+    const name = input.value;
+    this.model.set({ name });
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -22,7 +38,7 @@ export class UserForm {
         <div>${this.model.get("name")}</div>
         <div>${this.model.get("age")}</div>
         <input/>
-        <button>Click me!</button>
+        <button class='set-name'>Change name</button>
         <button class='set-age'>Set random age</button>
     </div>
     `;
@@ -40,6 +56,7 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
