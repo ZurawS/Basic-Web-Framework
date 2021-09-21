09a5839d0f825bc695056e5815f1936026851398
PawZet <pawelzurawski898@gmail.com>
2021-09-21 22:30:57 +0200

    commit_5

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,5 +1,17 @@
+import { User } from "./../models/User";
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:.set-age": this.onSetAgeClick,
+    };
+  }
+
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
@@ -7,15 +19,30 @@ export class UserForm {
         <h1>
              User Form
         </h1>
+        <div>${this.model.get("name")}</div>
+        <div>${this.model.get("age")}</div>
         <input/>
         <button>Click me!</button>
+        <button class='set-age'>Set random age</button>
     </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
