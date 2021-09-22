91e6862bfe74722846d6ea2137f98069856988d3
PawZet <pawelzurawski898@gmail.com>
2021-09-22 19:33:40 +0200

    commit_6

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
-import { UserForm } from "./views/UserForm";
 
 const user = User.buildUser({ name: "name", age: 20 });
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  // console.log(userEdit);
 } else {
   throw new Error(`Root element not found`);
 }
